dd76d974083669e6e89a8c6c014a6e64a1a738a2
Katyayan aanand <katyayan.aanand@gmail.com>
2019-07-23 00:31:56 +0530

    reduced questions and minor changes

---
+++ question.js
@@ -0,0 +1,30 @@
+var questions=[{
+    "question":"What is the Capital of Bangladesh?",
+    "option1":"Rajasthan",
+    "option2":"Dhaka",
+    "option3":"Khulna",
+    "option4":"Chittagong",
+    "answer":"2"
+}, {
+    "question":"C++ is a",
+    "option1":"Just a Language",
+    "option2":"Football",
+    "option3":"Programming Language",
+    "option4":"Rice",
+    "answer": "3"
+},{
+    "question":"Web pages are written using ",
+    "option1":"URL",
+    "option2":"HTTP",
+    "option3":"FTP",
+    "option4":"IP",
+    "answer":"1"
+}, {
+    "question":"Who is god",
+    "option1":"Mark",
+    "option2":"Anis",
+    "option3":"Katyayan",
+    "option4":"Uday",
+    "answer":"3"
+}
+]
